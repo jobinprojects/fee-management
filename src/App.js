88fdcd68e97d64869322630ba8e9067bb1211f16
jobinprojects <134594052+jobinprojects@users.noreply.github.com>
2023-06-06 12:17:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import AboutPage from './components/AboutPage';
 import ServicesPage from './components/ServicesPage';
 import ContactPage from './components/ContactPage';
+import FAQ from './components/Faq';
+
 
 const Header = () => {
   return (
@@ -15,6 +17,7 @@ const Header = () => {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/faq">FAQ</Link></li> 
         </ul>
       </nav>
     </header>
@@ -25,10 +28,10 @@ const Footer = () => {
   return (
     <footer className="footer">
       <ul className="social-links">
-        <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-        <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+        <li><a href="https://www.facebook.com"><i className="fab fa-facebook"></i></a></li>
+        <li><a href="https://www.twitter.com"><i className="fab fa-twitter"></i></a></li>
+        <li><a href="https://www.instagram.com"><i className="fab fa-instagram"></i></a></li>
+        <li><a href="https://www.linkedin.com"><i className="fab fa-linkedin"></i></a></li>
       </ul>
     </footer>
   );
@@ -44,11 +47,14 @@ const App = () => {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/faq" element={<FAQ />} />
         </Routes>
+        
         <Footer />
       </div>
     </Router>
   );
 };
+
 
 export default App;
